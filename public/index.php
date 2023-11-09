fb8491b903e5d6d708daf01b078b2f49b9755916
dimson <dimson-kaba@mail.ru>
2023-11-09 20:19:26 +0300

    Добавление пары маршрутов

--- public/index.php
+++ public/index.php
@@ -2,16 +2,28 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Factory\AppFactory;
 
 $app = AppFactory::create();
+
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function ($request, $response) {
+$app->get('/', function (Request $request, Response $response) {
     $response->getBody()->write('Welcome to Slim!');
     return $response;
     // Благодаря пакету slim/http этот же код можно записать короче:
     // return $response->write('Welcome to Slim!');
+});
+
+$app->get('/users', function (Request $request, Response $response) {
+    $response->getBody()->write('GET /users');
+    return $response;
+});
+
+$app->post('/users', function (Request $request, Response $response) {
+    return $response->withStatus(302);
 });
 
 $app->run();
