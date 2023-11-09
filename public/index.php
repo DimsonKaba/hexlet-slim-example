9797592d451ba86d57373ac06320f4c83cb76804
dimson <dimson-kaba@mail.ru>
2023-11-10 00:04:19 +0300

    Добавление работы с шаблонами

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ require __DIR__ . '/../vendor/autoload.php';
 use Psr\Http\Message\RequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Factory\AppFactory;
+use DI\Container;
 
 $app = AppFactory::create();
 
@@ -17,13 +18,39 @@ $app->get('/', function (Request $request, Response $response) {
     // return $response->write('Welcome to Slim!');
 });
 
-$app->get('/users', function (Request $request, Response $response) {
-    $response->getBody()->write('GET /users');
-    return $response;
-});
+// $app->get('/users', function (Request $request, Response $response) {
+//     $response->getBody()->write('GET /users');
+//     return $response;
+// });
 
 $app->post('/users', function (Request $request, Response $response) {
     return $response->withStatus(302);
 });
+
+$app->get('/courses/{id}', function (Request $request, Response $response, array $args) {
+    $id = $args['id'];
+    $response->getBody()->write("Course id: {$id}");
+    return $response;
+});
+
+//////
+
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+$app = AppFactory::createFromContainer($container);
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/users/{id}', function (Request $request, Response $response, array $args) {
+    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
+    // $this доступен внутри анонимной функции благодаря https://php.net/manual/ru/closure.bindto.php
+    // $this в Slim это контейнер зависимостей
+    return $this->get('renderer')->render($response, '/users/show.phtml', $params);
+});
+
+//////
 
 $app->run();
